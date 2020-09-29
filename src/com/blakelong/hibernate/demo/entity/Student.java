2636abaa60530591802ab4bf0a9aeddde6624c35
Blake A. Long <blake.a.long@gmail.com>
2020-09-29 10:53:54 -0600

    constructor and getters and setters

--- src/com/blakelong/hibernate/demo/entity/Student.java
+++ src/com/blakelong/hibernate/demo/entity/Student.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import sun.util.resources.cldr.ext.TimeZoneNames_hsb;
+
 @Entity
 // @Table is optional as long as class and table name are the same
 @Table(name="student")
@@ -23,4 +25,53 @@ public class Student {
 	
 	@Column(name="email")
 	private String email;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
+	
 }
