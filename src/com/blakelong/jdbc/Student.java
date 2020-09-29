f81a9b502e3e20e168f1fa2022fec7b41dcd2c23
Blake A. Long <blake.a.long@gmail.com>
2020-09-29 10:45:03 -0600

    field annotations

--- src/com/blakelong/jdbc/Student.java
+++ src/com/blakelong/jdbc/Student.java
@@ -1,6 +1,8 @@
 package com.blakelong.jdbc;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -8,4 +10,17 @@ import javax.persistence.Table;
 @Table(name="student")
 public class Student {
 	
+	// @Column is optional if column and field name are exactly the same
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
 }
